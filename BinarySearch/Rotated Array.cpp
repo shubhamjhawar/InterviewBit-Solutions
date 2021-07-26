dd52aad9d5a3fb02a3df9a62c4d2447e2913af07
shubhamjhawar <jhawarshubhamcs@gmail.com>
2021-07-26 17:57:16 +0530

    "Sample Binary Search Questions"

---
+++ BinarySearch/Rotated Array.cpp
@@ -0,0 +1,30 @@
+//Rotated Array
+//https://www.interviewbit.com/problems/rotated-array/
+
+int Solution::findMin(const vector<int> &A)
+{
+
+  //F*T*
+  //p(x) = nums[x] < nums[0]
+  //First T
+  int mid, low, high;
+  low = 0;
+  int n = A.size();
+  high = n - 1;
+
+  while (low < high)
+  {
+
+    mid = low + (high - low) / 2;
+
+    if (A[mid] < A[0])
+      high = mid;
+    else
+      low = mid + 1;
+  }
+
+  if (A[low] < A[0])
+    return A[low];
+  else
+    return A[0];
+}
